7241f5de874a965aa444c247ebeb36a85cb25546
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-07-19 20:21:20 +0700

    add more request method in method decorator

--- decorator/requestMethod.ts
+++ decorator/requestMethod.ts
@@ -1,7 +1,25 @@
-import { MetaData } from '../metadata/index.ts';
+import { MetaData } from '../metadata.ts';
 
 export const GET = (url?: string) => {
   return (target: any, key: string, descriptor: PropertyDescriptor) => {
-    MetaData.addMethod(target.constructor.name, url || '', descriptor.value);
+    MetaData.addMethod('GET', target.constructor.name, url || '', descriptor.value);
+  };
+};
+
+export const POST = (url?: string) => {
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    MetaData.addMethod('POST', target.constructor.name, url || '', descriptor.value);
+  };
+};
+
+export const PUT = (url?: string) => {
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    MetaData.addMethod('PUT', target.constructor.name, url || '', descriptor.value);
+  };
+};
+
+export const DELETE = (url?: string) => {
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    MetaData.addMethod('DELETE', target.constructor.name, url || '', descriptor.value);
   };
 };
