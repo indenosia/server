a2af728e8929b5000ccee07f415529ba6e77bbcd
chandra agung rizky <needkopi@gmail.com>
2020-09-22 16:39:33 +0000

    add example documentation in code

--- decorator/controller.ts
+++ decorator/controller.ts
@@ -1,3 +1,23 @@
+// this file for read decorator in class
+//
+// its same with prefix route example you 
+// have route
+// - /product/
+// - /product/detail
+//
+// you just to create 
+//
+// @Controller('/product')
+// class Product{
+//   @Get()
+//    TODO something
+//   @Get("/detail")
+//    TODO something
+// }
+//
+// see https://www.typescriptlang.org/docs/handbook/decorators.html
+// for documentation decorator typescript
+
 import { MetaData } from '../metadata.ts';
 
 export const Controller = (url?: string) => {
